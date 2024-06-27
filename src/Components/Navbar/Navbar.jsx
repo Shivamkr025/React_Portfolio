782d3901f974151f1c85bfb041ce686d2f7f19e1
Shivamkr025 <shivam22@navgurukul.org>
2024-06-27 23:30:58 +0530

    add navbar for a portfolio project

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-md navbar-light bg-light py-3 mb-4">
+            <div className="container">
+
+                <i class="fa-regular fa-face-smile" style={{ color: "rgb(227, 200, 255)", fontSize: '1.5rem' }}></i>
+                <span className="fs-4 text-danger" style={{ fontWeight: '600' }}>CONSULT</span>
+
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item "><a href="#" className="nav-link text-danger" aria-current="page">Home</a></li>
+                        <li className="nav-item"><a href="#" className="nav-link">ABOUT</a></li>
+                        <li className="nav-item"><a href="#" className="nav-link">SKILL</a></li>
+                        <li className="nav-item"><a href="#" className="nav-link">PROJECT</a></li>
+                        <li className="nav-item"><a href="#" className="nav-link">CONTACT</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
