8be4c8049d016756d764ddceab0630ca988c47a1
Shivamkr025 <shivam22@navgurukul.org>
2024-06-28 23:19:33 +0530

    update a navbar add scroller in navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,30 +1,112 @@
-import React from 'react'
+import React , {useEffect} from 'react';
+import { Link, animateScroll as scroll } from "react-scroll";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './Navbar.css'; 
 
 function Navbar() {
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const navbar = document.querySelector('.navbar');
+            if (window.scrollY > 50) {
+                navbar.classList.add('scrolled');
+            } else {
+                navbar.classList.remove('scrolled');
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <nav className="navbar navbar-expand-md navbar-light bg-light py-3 mb-4">
+        <nav className="navbar navbar-expand-md navbar-light py-3">
             <div className="container">
 
-                <i class="fa-regular fa-face-smile" style={{ color: "rgb(227, 200, 255)", fontSize: '1.5rem' }}></i>
-                <span className="fs-4 text-danger" style={{ fontWeight: '600' }}>CONSULT</span>
+                <i className="fa-regular fa-face-smile" style={{ color: "rgb(227, 200, 255)", fontSize: '1.5rem' }}></i>
+                <span className="fs-4 text-danger" style={{ fontWeight: '600', marginLeft: '8px' }}>CONSULT</span>
 
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
+
                 <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
                     <ul className="navbar-nav">
-                        <li className="nav-item "><a href="#" className="nav-link text-danger" aria-current="page">Home</a></li>
-                        <li className="nav-item"><a href="#" className="nav-link">ABOUT</a></li>
-                        <li className="nav-item"><a href="#" className="nav-link">SKILL</a></li>
-                        <li className="nav-item"><a href="#" className="nav-link">PROJECT</a></li>
-                        <li className="nav-item"><a href="#" className="nav-link">CONTACT</a></li>
+                        <li className="nav-item">
+                            <Link
+                                activeClass="active"
+                                to="home"
+                                spy={true}
+                                smooth={true}
+                                offset={-50}
+                                duration={500}
+                                className="nav-link text-danger"
+                            >
+                                <i className="bi bi-house"></i> Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                activeClass="active"
+                                to="about"
+                                spy={true}
+                                smooth={true}
+                                offset={-50}
+                                duration={500}
+                                className="nav-link"
+                            >
+                                <i className="bi bi-person"></i> ABOUT
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                activeClass="active"
+                                to="skill"
+                                spy={true}
+                                smooth={true}
+                                offset={-50}
+                                duration={500}
+                                className="nav-link"
+                            >
+                                SKILL
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                activeClass="active"
+                                to="project"
+                                spy={true}
+                                smooth={true}
+                                offset={-50}
+                                duration={500}
+                                className="nav-link"
+                            >
+                                PROJECT
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                activeClass="active"
+                                to="contact"
+                                spy={true}
+                                smooth={true}
+                                offset={-50}
+                                duration={500}
+                                className="nav-link"
+                            >
+                                CONTACT
+                            </Link>
+                        </li>
                     </ul>
                 </div>
             </div>
         </nav>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
