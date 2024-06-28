2a357f30cf3a22b47ff04970c6ee24e5ba8dd0d7
Shivamkr025 <shivam22@navgurukul.org>
2024-06-28 23:20:26 +0530

    update home page add icon

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,13 +4,17 @@ import './Home.css'
 
 function Home() {
     return (
-        <div className="container">
+        <div className="home-mainContainer mb-4">
             <div className="row align-items-center home-container">
-                <div className="col-md-6 text-center text-md-left">
+                <div className="col-md-6 text-center text-md-left text-Container">
                     <p>Hill, I am Shivam Kumar</p>
                     <h1>I'm a Developer!</h1>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-                    <button className="btn btn-primary">Resume</button>
+                    <button className="btn home-btn"><i class="bi bi-github home-icon" ></i></button>
+                    <button className="btn home-btn"><i class="bi bi-linkedin home-icon"></i></button>
+                    <button className="btn home-btn"><i class="bi bi-instagram home-icon"></i></button>
+                    <button className="btn home-btn"><i class="bi bi-file-earmark-person home-icon"></i></button>
+                    <button className="btn home-btn"><i class="bi bi-chat-right-text-fill home-icon"></i></button>
                 </div>
                 <div className="col-md-6 text-center">
                     <img src={img} alt="Profile Image" className="img-fluid" />
